519b8216ac41015c2f1143d602ea86a91eca8783
Clarence Chan <clarencechan123@gmail.com>
2021-08-12 15:33:44 -0400

    added tabs

--- src/index.js
+++ src/index.js
@@ -25,16 +25,41 @@ function header() {
     return headerBox;
 }
 
+function tabs() {
+    const tabBox = document.createElement('div');
+    const aboutTabBox = document.createElement('h3');
+    const menuTabBox = document.createElement('h3');
+    const contactTabBox = document.createElement('h3');
+    aboutTabBox.classList = "tab translucent";
+    menuTabBox.classList = "tab translucent";
+    contactTabBox.classList = "tab translucent";
+    aboutTabBox.innerHTML = "About Us";
+    menuTabBox.innerHTML = "Menu";
+    contactTabBox.innerHTML = "Contact"
+    tabBox.classList = "tab-box";
+    tabBox.appendChild(aboutTabBox);
+    tabBox.appendChild(menuTabBox);
+    tabBox.appendChild(contactTabBox);
+    return tabBox;
+
+}
+
 function about() {
-    const about = document.createElement('p');
-    about.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const aboutBox = document.createElement('div');
+    const aboutHeader = document.createElement('h2');
+    const about = document.createElement('p');
+    aboutHeader.innerHTML = "About Us"
+    about.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     about.classList = "about-text";
     aboutBox.classList = "translucent about-box";
+    aboutBox.appendChild(aboutHeader);
     aboutBox.appendChild(about);
     return aboutBox;
 }
 
-document.body.appendChild(component());
-document.body.appendChild(header());
-document.body.appendChild(about());
+const content = document.getElementById("content");
+
+content.appendChild(component());
+content.appendChild(header());
+content.appendChild(tabs());
+content.appendChild(about());
